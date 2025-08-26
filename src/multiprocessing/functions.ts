713e205dbbd2db852a1b81f1d2b63c27f7ece1e7
Samuele Casarin <eleumasc@gmail.com>
2025-08-26 15:02:22 +0200

    Add recoverScreenshots script

--- src/multiprocessing/functions.ts
+++ src/multiprocessing/functions.ts
@@ -1,7 +1,5 @@
 import { runAnalyze } from "../commands/cmdAnalyze";
-import { runLoginTaintAnalysisForRecoverScreenshots } from "../scripts/recoverScreenshots";
 
 export const MP_FUNCTIONS: Record<string, (...args: any[]) => any> = {
   runAnalyze,
-  runLoginTaintAnalysisForRecoverScreenshots,
 };
