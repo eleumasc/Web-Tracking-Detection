82a4bdf9bb90503763b1e0273a1e2069f7f3cff4
Samuele Casarin <eleumasc@gmail.com>
2026-04-14 14:41:02 +0000

    Add evalSystematicBreakage script

---
+++ src/scripts/evalSystematicBreakage.ts
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import { readFileSync } from "fs";
+import { TrackingRequestsFile } from "../commands/cmdMeasure";
+
+function main() {
+  const trackingRequestsFile = JSON.parse(
+    readFileSync("output/trackingRequests.relabeled.json").toString()
+  ) as TrackingRequestsFile;
+
+  const { entries } = trackingRequestsFile;
+  const requests = entries.flatMap((e) => e.trackingRequests);
+  const nonMatchingRequests = requests.filter(
+    (r) => r.syntacticVerifLabel === "NO_MATCHING_REQUESTS"
+  );
+
+  const nonMatchingTrackers = _.uniq(nonMatchingRequests.map((r) => r.tracker));
+  let coveredNonMatchingRequests = 0;
+
+  const systematicallyBrokenTrackers: string[] = [];
+  for (const targetTracker of nonMatchingTrackers) {
+    const targetRequests = requests.filter((r) => r.tracker === targetTracker);
+    if (targetRequests.length < 20) continue;
+
+    const targetNonMatchingRequests = nonMatchingRequests.filter(
+      (r) => r.tracker === targetTracker
+    );
+
+    if (targetNonMatchingRequests.length / targetRequests.length > 0.5) {
+      systematicallyBrokenTrackers.push(targetTracker);
+      coveredNonMatchingRequests += targetNonMatchingRequests.length;
+    }
+  }
+
+  console.log(systematicallyBrokenTrackers);
+  console.log(coveredNonMatchingRequests);
+}
+
+main();
